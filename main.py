35804e59dea33ecc3c68f27fc92952a22b021c7f
jsainz <jsainz@eecs.wsu.edu>
2021-04-06 16:21:34 -0700

    prototype files

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+
+read_file = pd.read_excel('C_stock_hydro_race_data.xlsx')
+read_file.to_csv('data.csv')
+df = pd.read_csv('data.csv')
+
+def normalize(x):
+    return x * (10**(-3))
+
+df['Head Temp [°F]'] = df['Head Temp [°F]'].apply(normalize)
+df['RPM [rpm]'] = df['RPM [rpm]'].replace("0,000",0.0)
+df['RPM [rpm]'] = df['RPM [rpm]'].apply(normalize)
+df['GPS_Speed [mph]'] = df['GPS_Speed [mph]'].apply(normalize)
+df['Prop Slip [% Slip]'] = df['Prop Slip [% Slip]'].apply(normalize)
+
+print(df)
