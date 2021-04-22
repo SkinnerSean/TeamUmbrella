aa784c44070f7ac092f589113790f6cdf5f5c0a3
jsainz <jsainz@eecs.wsu.edu>
2021-04-22 15:25:10 -0700

    Added some class files

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import pandas as pd
 
 read_file = pd.read_excel('C_stock_hydro_race_data.xlsx')
 read_file.to_csv('data.csv')
-df = pd.read_csv('data.csv')
+df = pd.read_csv('data.csv', index_col=1)
+df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
 
 def normalize(x):
+    if isinstance(x,str):
+        x = 0
     return x * (10**(-3))
 
-df['RPM [rpm]'] = df[df['RPM [rpm]'] != '0,000']
+    
+
+
 df['Head Temp [°F]'] = df['Head Temp [°F]'].apply(normalize)
 df['RPM [rpm]'] = df['RPM [rpm]'].apply(normalize)
 df['GPS_Speed [mph]'] = df['GPS_Speed [mph]'].apply(normalize)
