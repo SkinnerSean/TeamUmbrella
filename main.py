e19c2a82c687ffd42888badcfa7731dd28c5a71e
jsainz <jsainz@eecs.wsu.edu>
2021-04-21 22:24:25 -0700

    data base

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ df = pd.read_csv('data.csv')
 def normalize(x):
     return x * (10**(-3))
 
+df['RPM [rpm]'] = df[df['RPM [rpm]'] != '0,000']
 df['Head Temp [°F]'] = df['Head Temp [°F]'].apply(normalize)
-df['RPM [rpm]'] = df['RPM [rpm]'].replace("0,000",0.0)
 df['RPM [rpm]'] = df['RPM [rpm]'].apply(normalize)
 df['GPS_Speed [mph]'] = df['GPS_Speed [mph]'].apply(normalize)
 df['Prop Slip [% Slip]'] = df['Prop Slip [% Slip]'].apply(normalize)
