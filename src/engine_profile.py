707b744e490d43df17901d464e8cb13c545cdabb
jsainz <jsainz@eecs.wsu.edu>
2021-04-22 17:49:23 -0700

    Skeleton code

--- src/engine_profile.py
+++ src/engine_profile.py
@@ -1,14 +1,30 @@
-# import pandas as pd 
-
+import pandas as pd 
 # read_file = pd.read_excel('C_stock_hydro_race_data.xlsx')
 # read_file.to_csv('data.csv')
 # df = pd.read_csv('data.csv', index_col=1)
 # df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
-
 # def normalize(x):
 #     if isinstance(x,str):
 #         x = 0
 #     return x * (10**(-3))
+class EngineProfile:
+    def __init__(self,name,loc,file):
+        self.name = name
+        self.location = loc
+        self.csv = self.process_file(file)
+        self.stats = []
+
+    def process_file(self,file):
+        def normalize(x):
+            pass
+        pass
+
+    def new_race(self):
+        pass
+
+    def key_stats(self):
+        pass
+
 
     
 
