469118edffb7f820f984937d68b176e6016b4ad2
andrew <andrew_fritz@outlook.com>
2021-04-29 20:14:47 -0700

    added menu buttons

--- src/gui.py
+++ src/gui.py
@@ -4,6 +4,8 @@ import tkinter as tk
 class Menu1:
     def __init__(self, master):
         self.master = master
+        self.master.title("TEAM UMBRELLA")
+        self.master.geometry('350x200')
         self.frame = tk.Frame(self.master)
         self.title = tk.Label(
             self.frame,
@@ -15,7 +17,8 @@ class Menu1:
             width = 25,
             height = 5,
             bg = "grey",
-            fg = "black"
+            fg = "black",
+            command = self.view_window
         )
         self.button_new = tk.Button(
             self.frame,
@@ -23,16 +26,42 @@ class Menu1:
             width = 25,
             height = 5,
             bg = "grey",
-            fg = "black"
+            fg = "black",
+            command = self.new_window
         )
         self.title.pack()
         self.button_view.pack()
         self.button_new.pack()
         self.frame.pack()
 
+    def view_window(self):
+        self.newWindow = tk.Toplevel(self.master)
+        self.app = Menu2(self.newWindow)
 
-#class viewHydros
-#class newHydro
+    def new_window(self):
+        self.newWindow = tk.Toplevel(self.master)
+        self.app = Menu3(self.newWindow)
+
+
+class Menu2:
+    def __init__ (self, master):
+        self.master = master
+        self.master.title("HYDROPLANES")
+        self.master.geometry('350x200')
+        self.frame = tk.Frame(self.master)
+        self.prompt = tk.Label(self.frame, text = "GUI->MENU->USERDATA")
+        self.prompt.pack()
+        self.frame.pack()
+
+class Menu3:
+    def __init__ (self, master):
+        self.master = master
+        self.master.title("ADD HYROPLANE")
+        self.master.geometry('350x200')
+        self.frame = tk.Frame(self.master)
+        self.prompt = tk.Label(self.frame, text = "COLLECT CSV -> MENU -> USER -> PROCESS CSV INTO PROFILE")
+        self.prompt.pack()
+        self.frame.pack()
 
 def main():
     root = tk.Tk()
